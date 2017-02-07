97b20cdbf699b808b94bfb3986de3f7f81f5f0cd
clim <cwlim1995@hotmail.com>
2017-02-07 13:50:18 +0100

    id24

---
+++ id24.py
@@ -0,0 +1,46 @@
+"""
+A permutation is an ordered arrangement of objects.
+
+For example, 3124 is one possible permutation of the digits 1, 2, 3 and 4.
+
+If all of the permutations are listed numerically or alphabetically, we call it lexicographic order.
+
+The lexicographic permutations of 0, 1 and 2 are:
+
+012   021   102   120   201   210
+
+What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
+"""
+
+
+def lexicographic_permutation_generator(number_list):
+    largest_index_k = -1
+    largest_index_l = 0
+    for k in range(len(number_list) - 1):
+        if number_list[k] < number_list[k+1]:
+            largest_index_k = k
+    if largest_index_k == -1:
+        return
+
+    for l in range(len(number_list)):
+        if number_list[largest_index_k] < number_list[l]:
+            largest_index_l = l
+
+    temp = number_list[largest_index_k]
+    number_list[largest_index_k] = number_list[largest_index_l]
+    number_list[largest_index_l] = temp
+
+    number_list[largest_index_k+1:] = reversed(number_list[largest_index_k+1:])
+    return number_list
+
+
+if __name__ == '__main__':
+    count = 1
+    result = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    while count <= 999999:
+        result = lexicographic_permutation_generator(result)
+        if result == None:
+            break
+        count += 1
+    print result
+
